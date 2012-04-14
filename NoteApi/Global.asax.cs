62ee8148508c8ad6bfe018f71ceaa625f0563b89
Andrew Browne <brownie@brownie.com.au>
2012-04-14 20:29:22 +1000

    Added nuget packages

--- NoteApi/Global.asax.cs
+++ NoteApi/Global.asax.cs
@@ -43,8 +43,6 @@ namespace NoteApi
         {
             AreaRegistration.RegisterAllAreas();
 
-            // Use LocalDB for Entity Framework by default
-            Database.DefaultConnectionFactory = new SqlConnectionFactory("Data Source=(localdb)\v11.0; Integrated Security=True; MultipleActiveResultSets=True");
 
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
