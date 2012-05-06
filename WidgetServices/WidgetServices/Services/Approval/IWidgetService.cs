332ad8fc2de439906f7bb31ae600e3ef99bafd7b
Andrew Browne <brownie@brownie.com.au>
2012-05-06 14:02:00 +1000

    Split widget classes into their own files

--- WidgetServices/WidgetServices/Services/Approval/IWidgetService.cs
+++ WidgetServices/WidgetServices/Services/Approval/IWidgetService.cs
@@ -2,9 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-
-    using NHibernate;
 
     public interface IWidgetService
     {
@@ -13,36 +10,5 @@
         WidgetDetails GetWidgetDetails(Guid id);
 
         IEnumerable<WidgetDetails> GetWidgets();
-    }
-
-    public class WidgetDetails
-    {
-        public virtual Guid ApprovalId { get; set; }
-        public virtual string Title { get; set; }
-    }
-
-    class WidgetService : IWidgetService
-    {
-        private readonly ISession _session;
-
-        public WidgetService(UnitOfWork unitOfWork)
-        {
-            this._session = unitOfWork.Session;
-        }
-
-        public void SetWidgetDetails(WidgetDetails widgetDetails)
-        {
-            _session.SaveOrUpdate(widgetDetails);
-        }
-
-        public WidgetDetails GetWidgetDetails(Guid id)
-        {
-            return _session.Get<WidgetDetails>(id);
-        }
-
-        public IEnumerable<WidgetDetails> GetWidgets()
-        {
-            return _session.CreateCriteria<WidgetDetails>().List<WidgetDetails>().ToList();
-        }
     }
 }
---
+++ WidgetServices/WidgetServices/Services/Approval/WidgetDetails.cs
@@ -0,0 +1,10 @@
+namespace WidgetServices.Services.Approval
+{
+    using System;
+
+    public class WidgetDetails
+    {
+        public virtual Guid ApprovalId { get; set; }
+        public virtual string Title { get; set; }
+    }
+}
