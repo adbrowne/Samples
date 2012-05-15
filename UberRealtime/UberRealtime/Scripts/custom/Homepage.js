8749cac84882070bc942f6c63bc2a1e6b200e312
Andrew Browne <brownie@brownie.com.au>
2012-05-15 22:12:00 +1000

    Added Uber Realtime Sample

---
+++ UberRealtime/UberRealtime/Scripts/custom/Homepage.js
@@ -0,0 +1,8 @@
+﻿// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
+function AppViewModel() {
+    this.firstName = "Bert";
+    this.lastName = "Bertington";
+}
+
+// Activates knockout.js
+ko.applyBindings(new AppViewModel());
